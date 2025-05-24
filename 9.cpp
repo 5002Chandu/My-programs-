f164716b6af26eb2cf4188f8f640ef4242f01962
unknown <chanducharan2006@gmail.com>
2025-05-24 15:40:55 +0530

    initial commit

---
+++ 9.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class A
+{
+    private:
+         int kills = 69;
+         string name =" alice ";
+     friend void access(A obj);
+};
+void access (A obj)
+{
+    cout<<"The Ninja "<<obj.name;
+    cout<<"has killed "<<obj.kills<<" enemies"<<endl;
+}
+int main()
+{
+    A obj;
+    access(obj);
+    return 0;
+}
